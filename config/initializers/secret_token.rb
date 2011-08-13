e093956a356de3b12faa08a9a8a59a959f1ec6d1
Sergio Rafael Gianazza <sergio.gianazza@teracode.com>
2011-08-12 21:04:38 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouldwatch::Application.config.secret_token = '9b6e6186f0e0a199c89accd931fe0723dcd2aa6e960ee6c373ee6236af39ea2518f4bff04036f21f5995df315b19cfee977c83f311c8ed63f5159bbec9ee6bae'
