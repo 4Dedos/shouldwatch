fb4de6e616ae74cc9c1a25f01a21712a95b4c844
jmrepetti <jmrepetti@gmail.com>
2011-08-13 20:05:13 -0300

    Add to recommended movies

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,9 +1,10 @@
 class RecommendationsController < ApplicationController
 
   def show
-    prepare_lists
-    @recommendation = Recommendation.new
+    @recommendation = Recommendation.find(params[:id])
+    current_user.accept_recomendation(@recommendation)
     @popup_template = 'recommendations/show'
+    prepare_lists    
     render :template => 'home/index'
   end
 
