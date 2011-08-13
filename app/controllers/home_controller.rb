5326b0ec9550330a2f5d2bcc94d678ba0345c44f
Nicolas Gaivironsky <nicolas.gaivironsky@teracode.com>
2011-08-13 16:45:50 -0300

    Partial for movies in the lists

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
 
   def index
+    @want_to_watch = [
+        "The Shawshank Redemption (1994)",
+        "The Godfather (1972)",
+        "The Godfather: Part II (1974)",
+        "The Good, the Bad and the Ugly (1966)",
+        "Pulp Fiction (1994)",
+        "12 Angry Men (1957)",
+        "Schindler's List (1993)",
+        "One Flew Over the Cuckoo's Nest (1975)",
+        "Inception (2010)",
+        "The Dark Knight (2008)",
+        "The Lord of the Rings: The Return of the King (2003)"
+    ].collect{|m| Movie.new(:title => m)}
 
+    @i_recommend = @want_to_watch
+    @recommended_to_me = @want_to_watch
   end
 
   def first_visit
