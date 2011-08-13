5ddbf9e40bea909323d4129224f2eed4dca56fd9
Sergio Rafael Gianazza <sergio.gianazza@teracode.com>
2011-08-13 10:26:16 -0300

    Added specs to user
    Added a spec (and code) that return a list of movies that i should
    watch!

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,19 @@ class User < ActiveRecord::Base
       authentication_token.user
     end
   end
+
+  def i_should_watch_list
+    ret = []
+    should_watch_movies.each do |movie|
+      rotten_movie = RottenMovie.find(:id => movie.movie_id)
+      movie_hash = {}
+      movie_hash[:id] = movie.id
+      movie_hash[:title] = rotten_movie.title
+      movie_hash[:year] = rotten_movie.year
+      ret << movie_hash
+    end
+   
+    return ret
+  end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,25 @@ describe User do
       @user2.recommended_to_me.count.should == 1
       @user2.recommended_to_me[0].movie_id.should == "gone_with_the_wind"
     end
+
+  end
+
+  context "#I should watch" do
+    before (:each) do
+      @user = User.new(:name => "gianu", :avatar => "http://www.google.com", :email => "none@noneland.com")
+      @user.stubs(:should_watch_movies).returns([ShouldWatchMovie.new(:movie_id => "1"), ShouldWatchMovie.new(:movie_id => "2")])
+    end
+
+    it "return a list of movies I should watch" do
+      RottenMovie.expects(:find).with(:id => "1").returns(OpenStruct.new({:title => "Rocky I", :year => "1981"}))
+      RottenMovie.expects(:find).with(:id => "2").returns(OpenStruct.new({:title => "The Godfather", :year => "1978"}))
+      list = @user.i_should_watch_list
+      list.count.should == 2
+      list[0][:title].should == "Rocky I"
+      list[0][:year].should == "1981"
+      list[1][:title].should == "The Godfather"
+      list[1][:year].should == "1978"
+    end
   end
 
 end
