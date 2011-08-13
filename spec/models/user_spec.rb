349533585337c5dda7e7e1a8960aaa1cbdb62aca
Sergio Rafael Gianazza <sergio.gianazza@teracode.com>
2011-08-12 22:34:52 -0300

    Agregue el primer spec
    Agregue el primer spec y elimine los spec pending

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#I recommend" do
+    before (:each) do
+      @user = User.new(:name => "gianu", :avatar => "http://www.google.com", :email => "none@noneland.com")
+      @user2 = User.new(:name => "mati", :avatar => "http://www.yahoo.com", :email => "other_none@noneland.com")
+      @user3 = User.new(:name => "fer", :avatar => "http://www.altavista.com", :email => "another_none@noneland.com")
+      @user4 = User.new(:name => "nico", :avatar => "http://www.bing.com", :email => "last_none@noneland.com")
+    end
+    
+    it "return a list of movies that I recommend" do
+      accepted_recommendation = AcceptedRecommendation.new
+      accepted_recommendation.user_origin = @user
+      accepted_recommendation.user_destination = @user2
+      accepted_recommendation.movie_id = "gone_with_the_wind"
+      accepted_recommendation.added = false
+      accepted_recommendation.save!
+
+      @user.i_recommend.count.should == 1
+      @user.i_recommend[0].movie_id.should == "gone_with_the_wind"
+      @user2.recommended_to_me.count.should == 1
+      @user2.recommended_to_me[0].movie_id.should == "gone_with_the_wind"
+    end
+  end
 end
