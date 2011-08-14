8a689c21469bd2c321dbd3ebf844ffc291b4dd9f
Sergio Rafael Gianazza <sgianazza@gmail.com>
2011-08-14 19:11:33 -0300

    Minor change in text layout

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,11 +4,11 @@ module HomeHelper
   end
 
   def recommended_by(user)
-    link_to "from (@#{user})", "/#{user}"
+    link_to "(from @#{user})", "/#{user}"
   end
 
   def recommended_to(user)
-    link_to "to (@#{user})", "/#{user}"
+    link_to "(to @#{user})", "/#{user}"
   end
 end
 
