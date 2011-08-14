d63fac4965b586c619a8a6f801323283482ca56c
Sergio Rafael Gianazza <sgianazza@gmail.com>
2011-08-14 16:09:33 -0300

    Added a helper to write url

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,5 +2,13 @@ module HomeHelper
   def display_if(bool)
     "display: #{bool ? 'block' : 'none'}"
   end
+
+  def recommended_by(user)
+    link_to "from (@#{user})", "/#{user}"
+  end
+
+  def recommended_to(user)
+    link_to "to (@#{user})", "/#{user}"
+  end
 end
 
